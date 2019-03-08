f8a93c7a62058b161f634706c17b6a9cd3c88f47
Erik Szewczyk <erik@home.szfam.com>
2019-03-08 12:46:47 -0800

    Updated to use DarkSky API and misc

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# WeatherUnderground_PHP_MySQL
-PHP code for pulling Weather Forecasts from the Weather Underground API and caching the data in MySQL
+# DarkSky Weather API PHP plug-in with MySQL caching of forecast data
+PHP code for pulling Weather Forecasts from the DarkSky Weather API and caching the data in MySQL
 
-This code was developed to be used as an include which will display the forecast for the next 4 days.  In the code you'll need to leverage the Weather Underground forecast API which returns JSON, this then gets parsed by PHP and stored in a MySQL database so you dont have to go out to Weather Underground individually each time a page is refreshed. 
+![Example](Forecast-Example.PNG)
 
-The Weather Underground API can be found here, you'll need to get your own API key:
-https://www.wunderground.com/weather/api/
+This code was developed to be used as an include which will display the forecast for the next 8 days.  In the code you'll need to leverage the DarkSky forecast API which returns JSON, this then gets parsed by PHP and cached in a MySQL database so you dont have to go out to the API each time a page is refreshed.
 
-NOTE: You will need to manually add a location record to the weather table where the 'location' column matches the location id you specify in the php.  If you skip this step it will not be able to determine when the last update occured and you will get an error.
+There are 2 pages included:
+<ul>
+    <li>weather.php - a script meant to be included in your page(s) where you want the forecast to appear</li>
+    <li>weatherupdate.php - a script that when called will refresh the cached data, the intent here is that you would use something like a cron job to call this script on the frequency you prefer</li>
+</ul>
+
+The DarkSky API can be found here, you'll need to get your own API key:
+https://darksky.net/dev
+
+NOTE: You will need to manually add a location record to the weather table in MySQL where the 'location' column matches the location id you specify in the php.  The name you use is arbitrary, but needs to match on both ends.  The idea with this is that you could cache weather data from multiple locations if you desired.  If you skip this step it will not be able to determine when the last update occured and you will get an error.
--- weather.php
+++ weather.php
@@ -1,5 +1,9 @@
 <?php
 
+//Update these variables
+$location = "[Your Location ID]"; //The location name used in your table entry
+$latlong = "[37.8267,-122.4233]"; //The latitude and longitude of your location
+
 //Check if we have recent weather data in the DB, if not get it
     // Database credentials.
     $servername = "[MySQL Server]";
@@ -12,53 +16,8 @@
         die("Connection failed: " . mysqli_connect_error());
     }
     
-    //Determine how many minutes it's been since the last time the forecast has been updated
-    $lastupdate = mysqli_query($conn, "SELECT last_update FROM weather WHERE location = '[Your Unique Location ID]' ORDER BY id LIMIT 1");
-    while($row = mysqli_fetch_assoc($lastupdate)) { $last = $row['last_update']; };
-    $lastdt = new DateTime($last);
-    $now = new DateTime("now");
-    $interval = date_diff($lastdt, $now);
-    $minutes = $interval->days * 24 * 60;
-    $minutes += $interval->h * 60;
-    $minutes += $interval->i;
-//    echo $minutes.' minutes since last update<br>';
-    
-    //If the data is stale, get new data and update the record
-    if ($minutes > 15) {
-        //The API Key and URL below is from the Weather Underground documentation, you will need to replace the key and location
-        $json_string = file_get_contents("http://api.wunderground.com/api/e2aebab3a3ed8273/forecast/q/CA/San_Francisco.json");
-        $parsed_json = json_decode($json_string);
-        $conditions = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'};
-        $forecast = $parsed_json->{'forecast'}->{'simpleforecast'}->{'forecastday'};
-        $day0_weekday_short = $forecast[0]->date->weekday_short;
-        $day0_icon_url = $forecast[0]->icon_url;
-        $day0_high = $forecast[0]->high->fahrenheit;
-        $day0_low = $forecast[0]->low->fahrenheit;
-        $day1_weekday_short = $forecast[1]->date->weekday_short;
-        $day1_icon_url = $forecast[1]->icon_url;
-        $day1_high = $forecast[1]->high->fahrenheit;
-        $day1_low = $forecast[1]->low->fahrenheit;
-        $day2_weekday_short = $forecast[2]->date->weekday_short;
-        $day2_icon_url = $forecast[2]->icon_url;
-        $day2_high = $forecast[2]->high->fahrenheit;
-        $day2_low = $forecast[2]->low->fahrenheit;
-        $day3_weekday_short = $forecast[3]->date->weekday_short;
-        $day3_icon_url = $forecast[3]->icon_url;
-        $day3_high = $forecast[3]->high->fahrenheit;
-        $day3_low = $forecast[3]->low->fahrenheit;
-                
-        //Update in the db
-        $newdate= date('Y-m-d H:i:s');
-        $sql = "UPDATE weather SET last_update = '$newdate', day0_weekday_short = '$day0_weekday_short', day0_icon_url = '$day0_icon_url', day0_high = '$day0_high', day0_low = '$day0_low', day1_weekday_short = '$day1_weekday_short', day1_icon_url = '$day1_icon_url', day1_high = '$day0_high', day1_low = '$day1_low', day2_weekday_short = '$day2_weekday_short', day2_icon_url = '$day2_icon_url', day2_high = '$day2_high', day2_low = '$day2_low', day3_weekday_short = '$day3_weekday_short', day3_icon_url = '$day3_icon_url', day3_high = '$day3_high', day3_low = '$day3_low' WHERE location = '[Your Unique Location ID]'";
-        if (mysqli_query($conn, $sql)) {
-            //echo "OK";
-        } else {
-            echo "Fail: " . $sql . "<br/>" . mysqli_error($conn);
-        }
-    }
-    
     //Get the latest from the DB and set variables to be displayed
-    $weatherdata = mysqli_query($conn, "SELECT * FROM weather WHERE location = '[Your Unique Location ID]' ORDER BY id LIMIT 1");
+    $weatherdata = mysqli_query($conn, "SELECT * FROM weather WHERE location = $location ORDER BY id LIMIT 1");
     while($row = mysqli_fetch_assoc($weatherdata)) { 
         $location = $row['location'];
         $day0_weekday_short = $row['day0_weekday_short'];
@@ -77,41 +36,98 @@
         $day3_icon_url = $row['day3_icon_url'];
         $day3_high = $row['day3_high'];
         $day3_low = $row['day3_low'];
+        $day4_weekday_short = $row['day4_weekday_short'];
+        $day4_icon_url = $row['day4_icon_url'];
+        $day4_high = $row['day4_high'];
+        $day4_low = $row['day4_low'];
+        $day5_weekday_short = $row['day5_weekday_short'];
+        $day5_icon_url = $row['day5_icon_url'];
+        $day5_high = $row['day5_high'];
+        $day5_low = $row['day5_low'];
+        $day6_weekday_short = $row['day6_weekday_short'];
+        $day6_icon_url = $row['day6_icon_url'];
+        $day6_high = $row['day6_high'];
+        $day6_low = $row['day6_low'];
+        $day7_weekday_short = $row['day7_weekday_short'];
+        $day7_icon_url = $row['day7_icon_url'];
+        $day7_high = $row['day7_high'];
+        $day7_low = $row['day7_low'];
     };
 
     //Close connection
     mysqli_close($conn);
 ?>
 
-    <table align="center">
-        <tr>
-            <td class="fct_cell">
+		<div class="forecastday">
                 <?php
                     echo $day0_weekday_short;
-                    echo "<br><img src='" . $day0_icon_url . "'><br>";
+                    $date=date_create();
+                    //date_add($date,date_interval_create_from_date_string("1 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day0_icon_url . "' width=50 height=50></a><br>";
                     echo $day0_high . " / " .$day0_low;
                 ?>
-            </td>
-            <td class="fct_cell">
+		</div>
+		<div class="forecastday">
                 <?php
                     echo $day1_weekday_short;
-                    echo "<br><img src='" . $day1_icon_url . "'><br>";
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("1 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day1_icon_url . "' width=50 height=50></a><br>";
                     echo $day1_high . " / " .$day1_low;
                 ?>
-            </td>
-            <td class="fct_cell">
+		</div>
+		<div class="forecastday">
                 <?php
                     echo $day2_weekday_short;
-                    echo "<br><img src='" . $day2_icon_url . "'><br>";
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("2 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day2_icon_url . "' width=50 height=50></a><br>";
                     echo $day2_high . " / " .$day2_low;
                 ?>
-            </td>
-            <td class="fct_cell">
+		</div>
+		<div class="forecastday">
                 <?php
                     echo $day3_weekday_short;
-                    echo "<br><img src='" . $day3_icon_url . "'><br>";
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("3 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day3_icon_url . "' width=50 height=50></a><br>";
                     echo $day3_high . " / " .$day3_low;
                 ?>
-            </td>
-        </tr>
-    </table>
+		</div>
+        <div class="forecastday">
+                <?php
+                    echo $day4_weekday_short;
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("4 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day4_icon_url . "' width=50 height=50></a><br>";
+                    echo $day4_high . " / " .$day4_low;
+                ?>
+		</div>
+        <div class="forecastday">
+                <?php
+                    echo $day5_weekday_short;
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("5 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day5_icon_url . "' width=50 height=50></a><br>";
+                    echo $day5_high . " / " .$day5_low;
+                ?>
+		</div>
+        <div class="forecastday">
+                <?php
+                    echo $day6_weekday_short;
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("6 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day6_icon_url . "' width=50 height=50></a><br>";
+                    echo $day6_high . " / " .$day6_low;
+                ?>
+		</div>
+        <div class="forecastday">
+                <?php
+                    echo $day7_weekday_short;
+                    $date=date_create();
+                    date_add($date,date_interval_create_from_date_string("7 days"));
+                    echo "<br><a href='https://darksky.net/details/" . $latlong . "/" . date_format($date,"Y-n-j") . "/us12/en'><img src='" . $day7_icon_url . "' width=50 height=50></a><br>";
+                    echo $day7_high . " / " .$day7_low;
+                ?>
+		</div>
+?>
